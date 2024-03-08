701cfb3712dc046e14649de05f3b4278256d329a
Elxixau <indra90078@gmail.com>
2024-03-09 02:05:42 +0800

    update

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -45,84 +45,89 @@ export default function Form() {
    
       <div style={{display:'flex', justifyContent:'center', alignItems:'center', minHeight:'100vh'}}>
         <Container>
-          <Card className='mt-3 mb-3'>
-            <Card.Body className='p-5'>
-              <div className='img-group'>
-                <img src={logoKM} alt='' style={{}}></img>
-                <img src={logoUnmul} alt='' style={{marginLeft:'5px'}}></img>
-                <img src={logoPraktikum} alt='' style={{marginLeft:'5px'}} ></img>
-              </div>
-            <div className='row'>
-              <div className='col-lg-5'>
-                <img 
-                src={character} 
-                alt="student" 
-                className='img-fluid'
-                style={{ 
-                  borderRadius: '50%' 
-                }}>
-                </img>
-              </div>
-              <div className='col-lg-6'>
-                <h3>kumpulin<span>Tugas</span></h3>
-                <p className='text-muted'>Developed by Elxixau</p>
-                <div>
-                  {submissionSuccess && (
-                      <SuccessNotification/>
-                  )}
-                  {submissionError && (
-                    <ErrorNotification/>
-                  )}
-                  <form className="form mt-5" onSubmit={(e) => Submit(e)}>
-                    <div 
-                    className="form-group mb-4">
-                      <input 
-                      id="nama" 
-                      className="form-control mt-2 border-0 border-bottom" 
-                      placeholder="Nama Lengkap" 
-                      name="nama" 
-                      type="text" />
+            <Card className='mt-3 mb-3'>
+                <Card.Body className='p-5'>
+                    <div className='img-group'>
+                        <img src={logoKM} alt='' style={{}}></img>
+                        <img src={logoUnmul} alt='' style={{marginLeft:'5px'}}></img>
+                        <img src={logoPraktikum} alt='' style={{marginLeft:'5px'}} ></img>
                     </div>
-                    <div className="form-group mb-4">
-                      <input 
-                      id="nim" 
-                      className="form-control mt-2 border-0 border-bottom" 
-                      placeholder="NIM" 
-                      name="nim" 
-                      type="text" />
-                    </div>
-                    <div className="form-group mb-4">
-                      <select className="form-control mt-2 border-0 border-bottom" id="tugas_ke" placeholder="Tugas Ke-" name="tugas_ke">
-                        <option value="Tugas 1">Tugas 1</option>
-                        <option value="Tugas 2">Tugas 2</option>
-                        <option value="Tugas 3">Tugas 3</option>
-                        {/* Tambahkan opsi lainnya sesuai kebutuhan */}
-                      </select>
-                    </div>
-                    <div className="form-group mb-4">
-                      <input 
-                      id="url_repositori" 
-                      className="form-control mt-2 border-0 border-bottom" 
-                      placeholder="Link Github anda" 
-                      name="url_repositori" 
-                      type="text" />
-                    </div>
-                    <input 
-                    type="hidden" 
-                    name="waktu_input" 
-                    value={new Date().toLocaleString()} />
-                    <div className='mt-4' style={{display:'flex', alignItems:'center', justifyContent:'space-between'}}>
-                        <button className="btn btn-primary " type="submit">Submit</button>
-                        <div className='text-muted ' style={{fontSize:'12px', marginLeft:'20px'}}>
-                            "Cukup klik satu kali tombol submit dan tunggu hingga notifikasi muncul"
+                    <div className='row'>
+                        <div className='col-lg-5'>
+                            <img 
+                            src={character} 
+                            alt="student" 
+                            className='img-fluid'
+                            style={{ 
+                            borderRadius: '50%' 
+                            }}>
+                            </img>
+                        </div>
+                        <div className='col-lg-6'>
+                            <h3>kumpulin<span>Tugas</span></h3>
+                            <p className='text-muted'>Developed by Elxixau</p>
+                            <div>
+                            {submissionSuccess && (
+                                <SuccessNotification/>
+                            )}
+                            {submissionError && (
+                                <ErrorNotification/>
+                            )}
+                            <form className="form mt-5" onSubmit={(e) => Submit(e)}>
+                                <div 
+                                className="form-group mb-4">
+                                <input 
+                                id="nama" 
+                                className="form-control mt-2 border-0 border-bottom" 
+                                placeholder="Nama Lengkap" 
+                                name="nama" 
+                                type="text" />
+                                </div>
+                                <div className="form-group mb-4">
+                                <input 
+                                id="nim" 
+                                className="form-control mt-2 border-0 border-bottom" 
+                                placeholder="NIM" 
+                                name="nim" 
+                                type="text" />
+                                </div>
+                                <div className="form-group mb-4">
+                                <select className="form-control mt-2 border-0 border-bottom" id="tugas_ke" placeholder="Tugas Ke-" name="tugas_ke">
+                                    <option value="Tugas 1">Tugas 1</option>
+                                    <option value="Tugas 2">Tugas 2</option>
+                                    <option value="Tugas 3">Tugas 3</option>
+                                    {/* Tambahkan opsi lainnya sesuai kebutuhan */}
+                                </select>
+                                </div>
+                                <div className="form-group mb-4">
+                                <input 
+                                id="url_repositori" 
+                                className="form-control mt-2 border-0 border-bottom" 
+                                placeholder="Link Github anda" 
+                                name="url_repositori" 
+                                type="text" />
+                                </div>
+                                <input 
+                                type="hidden" 
+                                name="waktu_input" 
+                                value={new Date().toLocaleString()} />
+                                <div className='mt-4' style={{display:'flex', alignItems:'center', justifyContent:'space-between'}}>
+                                    <button className="btn btn-primary " type="submit">Submit</button>
+                                    <div className='text-muted ' style={{fontSize:'12px', marginLeft:'20px'}}>
+                                        "Cukup klik satu kali tombol submit dan tunggu hingga notifikasi muncul"
+                                    </div>
+                                </div>
+                            </form>
+                            </div>
                         </div>
                     </div>
-                  </form>
+                </Card.Body>
+            </Card>
+            <footer className="footer">
+                <div className="container text-center p-2">
+                    <span className="text-muted">&copy; {new Date().getFullYear()} KumpulinTugas_Visual Programming </span>
                 </div>
-              </div>
-            </div>
-            </Card.Body>
-          </Card>
+            </footer>
         </Container>
       </div>
   );
