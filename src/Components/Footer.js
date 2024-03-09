5688007ff50432ee32f48ff72859d666403ee40b
Elxixau <indra90078@gmail.com>
2024-03-09 12:58:45 +0800

    public update

---
+++ src/Components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer(){
+    return(
+        <footer className="footer text-center">
+                    <span className="text-muted">&copy; {new Date().getFullYear()} KumpulinTugas_Visual Programming</span>
+            </footer>
+    );
+}
+
+export default Footer;
